9c76dff216b3b67c135a559c64b1fd8855124fb5
Stanislaav Kozh <printks@yandex.ru>
2024-07-25 22:54:40 +0300

    Initial commit

---
+++ src/main/java/com/masya/systemcrmproductmicrosevice/SystemCrmProductMicroseviceApplication.java
@@ -0,0 +1,13 @@
+package com.masya.systemcrmproductmicrosevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemCrmProductMicroseviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemCrmProductMicroseviceApplication.class, args);
+    }
+
+}
